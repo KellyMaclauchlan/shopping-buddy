80be15611864baf931992a95f037426eca4b6365
AlexCLeduc <ledualex@gmail.com>
2017-05-30 19:13:54 -0400

    use better flex layout on app.js

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { AppRegistry, StyleSheet, Text, View, Button, Alert, TouchableHighlight, Image } from 'react-native';
+import { 
+  AppRegistry, 
+  StyleSheet, 
+  Text, 
+  View, 
+  Button, 
+  Alert, 
+  TouchableHighlight, 
+  Image, 
+} from 'react-native';
 import MenuItem from './components/MenuItem';
 import UnitPriceScreen from './components/UnitPriceScreen';
 import GroceryScreen from './components/GroceryScreen';
@@ -10,42 +19,54 @@ import RecipeScreen from './components/RecipeScreen';
 import { StackNavigator } from 'react-navigation';
 
 class App extends React.Component {
-static navigationOptions = {
+  static navigationOptions = {
     title: 'Welcome',
   };
   
-  groceryPress(){
-    Alert.alert(' grocery list Button has been pressed!');
-  };
-  expireDatePress(){
-    Alert.alert(' expire date Button has been pressed!');
-  };
-  unitConvertPress(){
-    Alert.alert(' unit convert Button has been pressed!');
-  };
-  pantryListPress(){
-    Alert.alert(' pantry list Button has been pressed!');
-  };
-  recipePlannerPress(){
-    Alert.alert(' recipe planner Button has been pressed!');
-  };
+//this is how alert works!  
+//Alert.alert(' recipe planner Button has been pressed!');
 
 
   render() {
     const { navigate } = this.props.navigation;
-    return (
+
+    return (  
       <View style={styles.container}>
-        <View style={{flexDirection:'row'}}>
-          <MenuItem onPress={() => navigate('UnitPrice')} image={require('./icons/scale.png')} text="Unit Price"/>
-          <MenuItem onPress={() => navigate('Grocery')} image={require('./icons/cart.png')} text="Grocery List"/>
+        <View style={styles.row}>
+          <MenuItem 
+            onPress={() => navigate('UnitPrice')} 
+            image={require('./icons/scale.png')} 
+            text="Unit Price"
+          />
+          <MenuItem 
+            onPress={() => navigate('Grocery')} 
+            image={require('./icons/cart.png')} 
+            text="Grocery List"
+          />
         </View>
-        <View style={{flexDirection:'row'}}>
-          <MenuItem onPress={() => navigate('Expire')} image={require('./icons/calendar.png')} text="Expirey Date"/>
-          <MenuItem onPress={() => navigate('UnitConvert')} image={require('./icons/retweet.png')} text="Unit Converter"/>
+        <View style={styles.row}>
+          <MenuItem 
+            onPress={() => navigate('Expire')} 
+            image={require('./icons/calendar.png')} 
+            text="Expirey Date"
+          />
+          <MenuItem 
+            onPress={() => navigate('UnitConvert')} 
+            image={require('./icons/retweet.png')} 
+            text="Unit Converter"
+          />
         </View>
-        <View style={{flexDirection:'row'}}>
-          <MenuItem onPress={() => navigate('Pantry')} image={require('./icons/notes.png')} text="Pantry List"/>
-          <MenuItem onPress={() => navigate('Recipe')} image={require('./icons/book.png')} text="Recipe Planner"/>
+        <View style={styles.row}>
+          <MenuItem  
+            onPress={() => navigate('Pantry')} 
+            image={require('./icons/notes.png')} 
+            text="Pantry List"
+          />
+          <MenuItem 
+            onPress={() => navigate('Recipe')} 
+            image={require('./icons/book.png')} 
+            text="Recipe Planner"
+          />
         </View>
       </View>
       );
@@ -53,11 +74,18 @@ static navigationOptions = {
 }
 
 const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#f9f9f9',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    flexDirection: "column",
   },
 });
 
